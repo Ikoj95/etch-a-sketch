5e9aa909b2d54792d267936e7099e28f8619f501
Jovan <jovanpavlovic2018@gmail.com>
2023-07-29 11:32:00 +0200

    added functionality to ask for number of rows and columns,and to remove existing sketch

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 const btn = document.querySelector('#btn');
 const sketch = document.querySelector('.sketch');
 const rmv = document.querySelector('#rmv');
+let numberOfRows = 0;
+let numberOfColumns = 0;
 
 
-function addingBoxes() {
-    for (let i = 0; i < 5; i++) {
-        let container = document.createElement('div');
-        container.classList.add('container');
-        for (let j = 0; j < 6; j++) {
-            let boxes = document.createElement('div');
-            boxes.classList.add('box');
-            container.appendChild(boxes);
+function askQuestion() {
+      while (true) {
+        numberOfRows = parseInt(prompt('Enter number of rows between 1-100'));
+        numberOfColumns = parseInt(prompt('Enter number of columns between 1-100'));
+        if(isNaN(numberOfRows) || isNaN(numberOfColumns)){
+            alert('you did not enter number')
+            return;
         }
-        sketch.appendChild(container);
+        else if ((numberOfRows >= 1 && numberOfRows <= 100) && (numberOfColumns >= 1 && numberOfColumns <= 100)) {
+            break;
+        }
+        else {
+            alert('wrong entry,only whole numbers between 1-100 are acceptable');
+        }
     }
 }
-btn.addEventListener('click',addingBoxes);
 
 
+function removeMe() {
 
-
-
-
-function removeMe(){
-    
     sketch.replaceChildren();
- 
-    /* second option
+
+    /* another way using loop
     let elementToRemove = document.getElementsByClassName('container');
     while(elementToRemove.length > 0){
         elementToRemove[0].parentNode.removeChild(elementToRemove[0]);
@@ -34,4 +35,24 @@ function removeMe(){
     */
 }
 
-rmv.addEventListener('click',removeMe)
+
+function addingBoxes() {
+    removeMe();
+    askQuestion();
+    for (let i = 0; i < numberOfRows; i++) {
+        let container = document.createElement('div');
+        container.classList.add('container');
+        for (let j = 0; j < numberOfColumns; j++) {
+            let boxes = document.createElement('div');
+            boxes.classList.add('box');
+            container.appendChild(boxes);
+        }
+        sketch.appendChild(container);
+    }
+}
+
+
+btn.addEventListener('click', addingBoxes);
+
+
+
